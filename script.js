3d5640e6dce84ba4e6edb80f3bc615f016210386
evaruppel <evaruppel08@gmail.com>
2024-02-09 16:05:25 +0500

    скрыть кнопку при скроле

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+let arrow = document.querySelector('.arrow')
+let body = document.querySelector('body')
 
+document.addEventListener('scroll', ()=>{
+    console.log(window.pageYOffset, window.innerHeight)
+})
