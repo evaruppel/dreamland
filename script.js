20e20655848119a26dcf4ddc2dcc5e1d33d4b60d
evaruppel <evaruppel08@gmail.com>
2024-02-16 15:27:11 +0500

    .

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 let arrow = document.querySelector('.arrow')
 let body = document.querySelector('body')
+let loc = document.querySelectorAll('.loc')
+let left_btn = document.querySelector('.leftbtn')
+let rigth_btn = document.querySelector('.rightbtn')
 
-document.addEventListener('scroll', ()=>{
-    console.log(window.pageYOffset, window.innerHeight)
+let i = 0
+rigth_btn.addEventListener('click', ()=>{
+    loc[i].classList.remove('active')
+    i += 1
+    if(i == loc.length - 1){i = 0}
+    loc[i].classList.add('active')
 })
+left_btn.addEventListener('click', ()=>{
+    loc[i].classList.remove('active')
+    if(i == 0){
+        i = loc.length - 1
+    }
+    i -= 1
+    loc[i].classList.add('active')
+})
+
+
+// document.addEventListener('scroll', ()=>{
+//     console.log(window.pageYOffset, window.innerHeight)
+// })
+
+
+
+// loc.forEach(element => {
+//     element.addEventListener('click', ()=>{
+//         element.classList.remove('active')
+//         i += 1
+//         if(i == loc.length - 1){i = 0}
+//         loc[i].classList.add('active')
+//     })
+// });
+
